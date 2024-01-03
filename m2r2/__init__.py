27c23d626e9e28cc21e5e86b87b921d650e2f5ad
daquintero <darioaquintero@gmail.com>
2024-01-03 13:13:32 +0100

    FIX: Does this sort out the import?

--- m2r2/__init__.py
+++ m2r2/__init__.py
@@ -1,6 +1,8 @@
 print("Importing from m2r2/__init__.py")
+from . import rst
 from .m2r2 import M2R, convert
 print("tryagain")
 from .sphinx.m2r2 import setup
 
-__all__ = ("M2R", "convert", "setup")
+
+__all__ = ("M2R", "convert", "setup", "rst")
